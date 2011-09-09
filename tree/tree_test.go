5aef1361201003c54ff274780fda87e4921ffccc
Christopher Piekarski <christopherpiekarski@yahoo.com>
2011-09-08 22:03:39 -0600

    added pre/post/in order walk tests

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -4,15 +4,44 @@ import (
 	"fmt"
 )
 
-func init() {
-	var 
+var root = BinaryNode{value:10}
+
+func init(){
+	BuildTree()
 }
 
-func TestNode(t *testing.T){
-	var n = Node{value:1}
-	n.Insert(&Node{value:0})
-	n.Insert(&Node{value:2})
-	fmt.Println(n,n.IsLeaf(),n.Size())
+func BuildTree(){
+	for i:=0;i < 20; i++ {
+		root.Insert(&BinaryNode{value:i})
+	}
+}
+
+func TestMin(t *testing.T){
+	fmt.Println(root.Minimum())
+}
+
+func TestMax(t *testing.T){
+	fmt.Println(root.Maximum())
+}
+
+func TestSize(t *testing.T){
+	fmt.Println(root.Size())
+}
+
+func printNode(n *BinaryNode){
+	fmt.Print(n.value," ")
+}
+
+func TestInorderWalk(t *testing.T){
+	root.InorderWalk(printNode)
+}
+
+func TestPostorderWalk(t *testing.T){
+	root.PostorderWalk(printNode)
+}
+
+func TestPreorderWalk(t *testing.T){
+	root.PreorderWalk(printNode)
 }
 
 func BenchmarkNode(b *testing.B){
