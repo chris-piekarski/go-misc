fa5abca6964f42a53de2ed95eb0a1de50025a1d0
Christopher Piekarski <chris@cpiekarski.com>
2013-07-16 22:59:06 -0600

    add FC REST API support

--- src/github.com/magnetize/mag.go
+++ src/github.com/magnetize/mag.go
@@ -3,7 +3,8 @@ package main
 import (
 	"time"
 	"fmt"
-	//"io/ioutil"
+    "encoding/json"
+	"io/ioutil"
 	"net/http"
 	"html/template"
 	"labix.org/v2/mgo"
@@ -14,6 +15,59 @@ type Giver struct {
 	Me string
 	NotMe string
 	Extra string
+}
+
+type NotMeDetails struct {
+	NotMe string
+	Time string
+}
+
+type GiverList struct {
+	Me string
+	NotMe []NotMeDetails
+}
+
+type FCStatus202 struct {
+	Message string
+	Status int
+	RequestId string
+	NotMe string
+}
+
+type FCStatus404 struct {
+	Message string
+	Status int
+	NotMe string
+	Me string
+}
+
+type FCContactInfo struct {
+	FamilyName string
+	FullName string
+	GivenName string
+	Websites []map[string] string
+	Chats []map[string] string
+	Organizations []map[string] string
+	Demographics map[string] string
+	SocialProfiles []map[string] string
+}
+
+type FCInfo struct {
+	Email string
+	Status int
+	Likelihood float32
+	RequestId string
+	Photos []map[string] string
+	ContactInfo FCContactInfo
+	DigitalFootprint map[string] string
+	Scores []map[string] string
+}
+
+func getFullContact(notMe string) *http.Response {
+	apiKey := "&apiKey=a42d9db67d03a3c7";
+	url := "https://api.fullcontact.com/v2/person.json?email=";
+	r, _ := http.Get(url+notMe+apiKey);
+	return r;
 }
 
 //https://api.fullcontact.com/v2/person.json?email=bart@fullcontact.com&apiKey=a42d9db67d03a3c7
@@ -31,32 +85,135 @@ func processPending() {
 		c := session.DB("pending").C("people")
 		err := c.Find(bson.M{}).One(&result)
 		if err == nil {
-			fmt.Printf("Processing %s for %s",result.NotMe, result.Me)
-			err = c.Remove(bson.M{"me":result.Me, "notme":result.NotMe})
+			fmt.Printf("Processing %s for %s\n",result.NotMe, result.Me)
+			
+			response := getFullContact(result.NotMe);
+			
+			defer response.Body.Close();
+        	contents, err := ioutil.ReadAll(response.Body);
+        	
+        	if response.StatusCode == 200 {
+	        	var fci FCInfo;
+				err = json.Unmarshal(contents, &fci);
+				fci.Email = result.NotMe;
+	        	fmt.Printf("%s likelihood %f\n",response.Status, fci.Likelihood);
+	        	storeFCEntry(fci, result.NotMe);
+	        	storeUserEntry(result.Me, result.NotMe);
+	        } else if response.StatusCode == 202 {
+	        	var fcs FCStatus202;
+				err = json.Unmarshal(contents, &fcs);
+				fcs.NotMe = result.NotMe;
+	        	fmt.Printf("%s likelihood %f\n",response.Status, fcs.Message);
+	        	storeQueuedEntry(fcs);
+	        } else {
+	        	var fcs FCStatus404;
+				err = json.Unmarshal(contents, &fcs);
+				fcs.NotMe = result.NotMe;
+				fcs.Me = result.Me;
+	        	fmt.Printf("%s %s\n",response.Status, fcs.Message);
+	        	storeFailedEntry(fcs);
+	        }
+        	
+			_, err = c.RemoveAll(bson.M{"me":result.Me, "notme":result.NotMe});
 			if err != nil {
-				fmt.Print(err)
+				fmt.Print(err);
 			}
 		}
 		
-		time.Sleep(10 * time.Second)
+		time.Sleep(10 * time.Second);
+	}
+}
+
+func storeUserEntry(me string, notMe string) {
+	result := GiverList{} 
+	session, err := mgo.Dial("127.0.0.1");
+	if err != nil {
+		panic(err);
+	}
+	defer session.Close();
+
+	session.SetMode(mgo.Monotonic, true);
+
+    c := session.DB("users").C("people");
+	err = c.Find(bson.M{"me":me}).One(&result)
+    fmt.Print("blah me");
+    newGiverDetails := NotMeDetails{notMe, time.Now().String()};
+    if err == nil {
+    	result.NotMe = append(result.NotMe, newGiverDetails );
+
+    	err = c.Update(bson.M{"me":me}, result);
+    } else {
+    	a := []NotMeDetails{newGiverDetails};
+    	c.Insert(&GiverList{me, a});
+    }
+    
+	if err != nil {
+		panic(err);
+	}
+}
+
+func storeFCEntry(fce FCInfo, notMe string) {
+	session, err := mgo.Dial("127.0.0.1");
+	if err != nil {
+		panic(err);
+	}
+	defer session.Close();
+
+	session.SetMode(mgo.Monotonic, true);
+
+    c := session.DB("contacts").C("people");
+    err = c.Insert(&fce);
+	if err != nil {
+		panic(err)
+	}
+}
+
+func storeFailedEntry(q FCStatus404) {
+	session, err := mgo.Dial("127.0.0.1");
+	if err != nil {
+		panic(err);
+	}
+	defer session.Close();
+
+	session.SetMode(mgo.Monotonic, true);
+
+    c := session.DB("pending").C("failed");
+    err = c.Insert(&q);
+	if err != nil {
+		panic(err)
+	}
+}
+
+func storeQueuedEntry(q FCStatus202) {
+	session, err := mgo.Dial("127.0.0.1");
+	if err != nil {
+		panic(err);
+	}
+	defer session.Close();
+
+	session.SetMode(mgo.Monotonic, true);
+
+    c := session.DB("pending").C("queued");
+    err = c.Insert(&q);
+	if err != nil {
+		panic(err)
 	}
 }
 
 func storeEntry(me string, notMe string, extra string) {
-	session, err := mgo.Dial("127.0.0.1")
+	session, err := mgo.Dial("127.0.0.1");
 	if err != nil {
-		panic(err)
+		panic(err);
 	}
-	defer session.Close()
+	defer session.Close();
 
-	session.SetMode(mgo.Monotonic, true)
+	session.SetMode(mgo.Monotonic, true);
 
-    c := session.DB("pending").C("people")
+    c := session.DB("pending").C("people");
     err = c.Insert(&Giver{me, notMe, ""})
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print("World")
 }
 
 func giveHandler(w http.ResponseWriter, r *http.Request) *Giver {
@@ -64,9 +221,6 @@ func giveHandler(w http.ResponseWriter, r *http.Request) *Giver {
     me := r.FormValue("me")
     notMe := r.FormValue("notMe")
 	g := &Giver{Me : me, NotMe : notMe, Extra : ""}
-	
-	//t, _ := template.ParseFiles("start.html")
-    //t.Execute(w, g)
     
     storeEntry(me, notMe, "")
     return g
@@ -77,12 +231,9 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	g := &Giver{Me : "", NotMe : "", Extra : ""}
 
 	if r.Method  == "POST" {
-		fmt.Print("Got a POST")
+		fmt.Print("Got a POST\n")
 		g = giveHandler(w, r)
 	}
-
-	//body, _ := ioutil.ReadFile("mag.html")
-	//w.Write(body)	
 	
 	t, _ := template.ParseFiles("mag.html")
     t.Execute(w, g)
@@ -90,7 +241,6 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/give", rootHandler)
-    //http.HandleFunc("/give/new", giveHandler)
     
     go processPending()
     fmt.Print("Starting server\n")
